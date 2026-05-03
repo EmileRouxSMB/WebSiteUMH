e6de9e1b05ed3caa6d7f8bf8a5cde2d2e82c2466
emileroux83 <emile.roux83@gmail.com>
2026-05-03 18:30:12 +0200

    maj

--- assets/js/umh-config.js
+++ assets/js/umh-config.js
@@ -2,7 +2,7 @@ window.UMH_CONFIG = {
   // URL du Web App Google Apps Script (meme URL pour GET JSON et POST formulaire)
   // Exemple:
   // apiUrl: "https://script.google.com/macros/s/AKfycb.../exec"
-  apiUrl: "https://script.google.com/macros/s/AKfycbybnUeIFoNl1IB7sjOTdKrIydzpN0eLtDequcUDBRI4DOOlK-t6URrRVinSEnXbiMza/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyeXxdDZGfmadKmnW904iHNown4wmnw9DrH4UEkaZ_gn6D5_th9XKPAxcAV-RawQ-gM/exec",
 
   // Liste des types visible dans nouveau-prestataire.html
   // C'est ici que tu modifies la liste manuellement.
