8e5910560ff890843546263beb20f2a6ee1f8df8
emileroux83 <emile.roux83@gmail.com>
2026-05-04 12:28:36 +0200

    maj umh-config.js

--- assets/js/umh-config.js
+++ assets/js/umh-config.js
@@ -1,14 +1,9 @@
 window.UMH_CONFIG = {
   // URL du Web App Google Apps Script (meme URL pour GET JSON et POST formulaire)
-  // Exemple:
-  // apiUrl: "https://script.google.com/macros/s/AKfycbysc0Fv5z-geFo95jtZNMiOE80Yo-bu680uGXLEY7SapQ3nlhBKR0tWHT9HyzLD1oRdAQ/exec"
   apiUrl: "https://script.google.com/macros/s/AKfycbyeXxdDZGfmadKmnW904iHNown4wmnw9DrH4UEkaZ_gn6D5_th9XKPAxcAV-RawQ-gM/exec",
+
 
   // Liste des types visible dans nouveau-prestataire.html
   // C'est ici que tu modifies la liste manuellement.
-  prestationOptions: [
-    "Photo",
-    "Lieu de reception",
-    "Traiteur"
-  ]
+  prestationOptions:  ["Lieu", "Photo / Vidéo", "Traiteur", "Coiffure", "Maquillage", "Animation", "Musique / DJ", "Décoration", "Officiant de cérémonie", "Service / Maitre d'hôtel", "Création robes de mariées", "Cadeaux personnalisés", "Papeterie", "Fleuriste", "Patisserie", "Autre"];
 };
